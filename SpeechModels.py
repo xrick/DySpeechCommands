11fc00b4b0bb31fa186ed5e6862531073458f0bd
Rick Liao <xrickliao@gmail.com>
2019-01-28 10:59:23 +0800

    commit the differences

--- SpeechModels.py
+++ SpeechModels.py
@@ -13,7 +13,8 @@ from kapre.utils import Normalization2D
 
 def SimpleDNN(nCategories, inputLength = 16000):
     model = Sequential()
-    #model.add(Dense(32, input_dim=16000, activation='relu'))
+    model.add(Dense(32, input_dim=16000, activation='relu'))
+    #model.add(BatchNormalization())
     model.add(Dense(32, activation='relu'))
     #model.add(BatchNormalization())
     model.add(Dense(32, activation='relu'))
